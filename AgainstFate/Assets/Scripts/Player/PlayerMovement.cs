717b1d82f3042f063535a50491dd31ac5ea462a1
Tatar3n <ivanchub2104@gmail.com>
2023-10-09 17:36:53 +0300

    Add Move

--- AgainstFate/Assets/Scripts/Player/PlayerMovement.cs
+++ AgainstFate/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,33 +4,54 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D _rigidbody;
-    private Vector2 _horizontalVelocity;
-    private float _horizontalSpeed;
+    private Rigidbody2D _rigidbody; // компонент rididbody
+    private Collider2D _collider;
+    private Vector2 _horizontalVelocity; // вектор(скорость) джижения
+    private float _horizontalSpeed; // направление движения
 
-    public float MoveSpeed; // скорость передвижения игрока
+    public float moveSpeed; // множитель скорости 
+    public float jumpForce; // множитель прыжка
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>(); // получаем объект Rigidbody игрока
+        _collider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         _horizontalSpeed = Input.GetAxis("Horizontal");
+        Jump();
     }
 
     // Здесь будет происходить перерасчёт физики
-	private void FixedUpdate()
-	{
+    private void FixedUpdate()
+    {
         Move();
+        Flip();
     }
 
     private void Move()
     {
-        _horizontalVelocity.Set(_horizontalSpeed * MoveSpeed, _rigidbody.velocity.y);
+        _horizontalVelocity.Set(_horizontalSpeed * moveSpeed, _rigidbody.velocity.y);
         _rigidbody.velocity = _horizontalVelocity;
     }
+
+    private void Flip()
+    {
+        if (_horizontalSpeed > 0)
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        else if (_horizontalSpeed < 0)
+            transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+            _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
 }
+
+
