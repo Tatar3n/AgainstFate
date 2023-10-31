6e591c75deb21b7002646ef97c076d490802a578
YBarsukova <yaroslavabarsukova@internet.ru>
2023-10-31 23:55:05 +0300

    I added a video stub as the initial scene and made a switch between scenes by clicking

---
+++ AgainstFate/Assets/Scripts/scenes.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class scenes : MonoBehaviour
+{
+    
+    public void change_to_testwalk(int n)
+        {
+        //SceneManager.LoadScene("TestWalk");
+        SceneManager.LoadScene(n);
+        }
+
+
+}
---
+++ AgainstFate/Assets/TestPrologue/button.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+;
+    }
+
+    void Update()
+    {
+        
+    }
+}
