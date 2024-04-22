c7a7cb6fd4f4ce059607e455b0d9ecfe7b774c4c
Amalia <115399488+privet-kak-dela@users.noreply.github.com>
2024-04-22 23:25:10 +0300

    Fix Rocks

--- AgainstFate/Assets/Scripts/Level2/RockTrapActivate.cs
+++ AgainstFate/Assets/Scripts/Level2/RockTrapActivate.cs
@@ -17,9 +17,11 @@ public class RockTrapActivate : MonoBehaviour
     public AdaptedPatterForCapricorn d;
     public GameObject savepoint;
     Respawn re;
-   
 
+    public GameObject rock;
+    private bool isPreparationEnd = true;
     private bool wait = true;
+    List<GameObject> rocks;
 
     private void Awake()
     {
@@ -28,30 +30,38 @@ public class RockTrapActivate : MonoBehaviour
             v[i] = p[i].transform.position;
             rb[i] = p[i].GetComponent<Rigidbody2D>();
         }
-        re = player.GetComponent<Respawn>();
+        re = player.GetComponent<Respawn>();  
     }
    
     private void Update()
     {
         if (d.IsEnd && !EndRockTrap)
         {
+           
             FallingGround.start = true;
             StartCoroutine(TimeBeforeRockTRap());
             if (Time.time - tt > 4 && !f)
             {
-
+               
                 f = true;
                 wait = false;
                 tt = Time.time;
-
             }
-            if (Time.time - t > 0.27 && f && ii < size && !wait)
+           
+
+            if (Time.time - t > 0.25 && f && ii < size && !wait)
             {
+                //if(isPreparationEnd)
+                //{
+                //    rocks.Add(Instantiate(rock, new Vector3(26.14f,-4.86f,0f), Quaternion.identity));
+                //    StartCoroutine(Waiting());
+                //}
+               
                 if (!CameraShake.startshake)
                     fl = true;
                 rb[ii].isKinematic = false;
                 ii++;
-              
+                
                 t = Time.time;
               
                 if (ii == rb.Length - 1 && player.transform.position.x <= savepoint.transform.position.x || player.transform.position.x <= savepoint.transform.position.x)
@@ -59,35 +69,35 @@ public class RockTrapActivate : MonoBehaviour
                     CameraShake.startshake = false;
                     EndRockTrap = true;
                 }
-
-
             }
             if (re.recoverRockTrap)
             {
+                //rocks.ForEach(Destroy);
                 fl = true;
                 wait = true;
                 ii = 0;
                 f = false;
+                
                 re.recoverRockTrap = false;
                 for (int i = 0; i < size; i++)
                 {
                     p[i].transform.position = v[i];
                     rb[i].bodyType = RigidbodyType2D.Static;
-
-                }
-              
-            }
-            
-           
-            
+                } 
+            }  
         }
-
         IEnumerator TimeBeforeRockTRap()
         {
             yield return new WaitForSeconds(3);
 
             f = true;
             wait = false;
+        }
+        IEnumerator Waiting()
+        {
+            isPreparationEnd = false;
+            yield return new WaitForSeconds(10f); // Задержка в 10 секунд
+            isPreparationEnd = true;
         }
 
 
